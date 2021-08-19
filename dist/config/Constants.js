b75e61ee8b57b0ef783fe505ce94d5fa1b8daf20
Ícaro Campos <82005138+icocampos@users.noreply.github.com>
2021-08-19 04:06:14 -0300

    Implementação do encurtador

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://icoDB:<password>@cluster0.qi8st.mongodb.net/url-shortener-ico?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION:
+    'mongodb+srv://icoDB:<password>@cluster0.qi8st.mongodb.net/url-shortener-ico?retryWrites=true&w=majority',
+}
